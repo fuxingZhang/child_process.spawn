76d0752bdd9c21447f6c9138eea4ef76b24d6a7b
star <502545703@qq.com>
2019-07-04 17:33:04 +0800

    add HighWaterMark

--- child_script.js
+++ child_script.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const stream = fs.createReadStream('./test.png');
-  
+
 stream.on('data', data => {
 	process.stdout.write(data)
 	// 这里不能用console.log
--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 
 const wirteStream = fs.createWriteStream('./result.png')
 
+console.log(wirteStream.writableHighWaterMark)  // 16384 => 16kb
+// console.log(readable.readableHighWaterMark)  // 65536 => 64kb
+
 ls.stdout.on('data', (data) => {
   console.log(data);
   console.log(`stdout type: ${typeof data}`);
-  console.log(`byteLength: ${Buffer.byteLength(data)}`);
+  console.log(`byteLength: ${Buffer.byteLength(data)}`); // 第一次64kb，第二次开始128kb，最后一次不定
   wirteStream.write(data)
 });
 
